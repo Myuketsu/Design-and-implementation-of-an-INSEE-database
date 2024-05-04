1c418073ae0ecd4efcdc7613f16df1aa94a973ac
Myuketsu <Myuketsu@gmail.com>
2024-05-04 15:11:34 +0200

    docstring

--- data/db_connector.py
+++ data/db_connector.py
@@ -12,10 +12,42 @@ __POOL: SimpleConnectionPool = SimpleConnectionPool(
     **load_config('database')
 )
 
-def execute_sql(query: str) -> DataFrame | None:
+def execute_sql(query: str, vars: tuple | list | dict) -> DataFrame | None:
+    """
+    Exécute une requête SQL avec des variables associées et renvoie les résultats sous forme de DataFrame pandas
+    ou None si la requête ne renvoie aucune donnée.
+
+    Args
+    ----
+        query (str): La requête SQL à exécuter.
+        vars (tuple | list | dict): Les variables associées à la requête, sous forme de tuple, liste ou dictionnaire.
+
+    Returns
+    -------
+        DataFrame | None: Un DataFrame pandas contenant les résultats de la requête si celle-ci renvoie des données, sinon None.
+
+    Remarques
+    ---------
+        Cette fonction utilise une connexion issue d'un pool de connexions pour exécuter la requête SQL. Une fois la
+        requête exécutée, la connexion est automatiquement libérée dans le pool. Si la requête ne renvoie aucune donnée
+        (par exemple, une requête INSERT, UPDATE ou DELETE), la fonction renvoie None.
+
+    See Also
+    --------
+        execute : Execute a database operation (query or command) (https://www.psycopg.org/docs/cursor.html#cursor.execute).
+
+    Exemple
+    -------
+        >>> # Exemple d'utilisation avec une requête SELECT
+        >>> execute_sql('SELECT * FROM table WHERE condition = %s;', (valeur,))
+        ...
+        >>> # Exemple d'utilisation avec une requête INSERT
+        >>> execute_sql('INSERT INTO table (colonne1, colonne2) VALUES (%s, %s);', (valeur1, valeur2))
+        ...
+    """
     conn: connection = __POOL.getconn()
     with conn.cursor() as cursor:
-        cursor.execute(query)
+        cursor.execute(query, vars)
 
         if cursor.description is None:
             conn.commit()
