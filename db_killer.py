8b4ef91a1eb1b0264289c1e03aac3c733331adcc
Myuketsu <Myuketsu@gmail.com>
2024-05-05 17:12:52 +0200

    minors changes

--- db_killer.py
+++ db_killer.py
@@ -1,7 +1,4 @@
-from data.db_connector import excute_sql_file, execute_sql, copy_to_sql
-from data.data_processing import select_rename_columns, DataFrame_to_buffer
-
-from pandas import read_csv
+from data.db_connector import excute_sql_file
 
 PATH_TO_SQL = './data/sql/'
 
