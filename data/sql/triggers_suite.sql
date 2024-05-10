62828b44744d90d7e8ad8514df5d02c94f91a69c
Myuketsu <Myuketsu@gmail.com>
2024-05-10 03:35:16 +0200

    Trigger_suite DONE !

--- data/sql/triggers_suite.sql
+++ data/sql/triggers_suite.sql
@@ -1,32 +1,86 @@
--- WIP
-CREATE OR REPLACE PROCEDURE update_population_if_all()
+-- On définit nos fonctions qui vont mettre à jour la population pour chaque département/région
+-- La population d'un/d'une département/région est calculée que si l'ensemble des communes/département du/(de la) département/région est renseignée.
+CREATE OR REPLACE FUNCTION calcul_pop_dep(annee_cible VARCHAR) 
+    RETURNS TABLE (
+        code_departement VARCHAR,
+        annee VARCHAR,
+		population FLOAT
+) AS $$
+BEGIN
+    RETURN QUERY 
+		SELECT C.code_departement, annee_cible AS annee, SUM(S.valeur) AS population
+		FROM commune C
+		JOIN (
+			SELECT code_commune, valeur 
+			FROM statistiques_pop
+			WHERE type_statistique = 'population' AND annee_debut = annee_cible
+		) S ON C.code = S.code_commune
+		JOIN (
+			SELECT TempC.code_departement, COUNT(*) AS commune_count -- Nombre total de commune pour chaque département
+			FROM commune TempC
+			GROUP BY TempC.code_departement
+		) CD ON C.code_departement = CD.code_departement
+		GROUP BY C.code_departement, commune_count
+		HAVING COUNT(*) = commune_count;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION calcul_pop_reg(annee_cible VARCHAR) 
+    RETURNS TABLE (
+        code_region VARCHAR,
+        annee VARCHAR,
+		population FLOAT
+) AS $$
+BEGIN
+    RETURN QUERY 
+		SELECT D.code_region, annee_cible AS annee, SUM(DP.pop) AS population
+		FROM departement D
+		JOIN (
+			SELECT TempDP.code_departement, TempDP.population AS pop
+			FROM departement_pop TempDP
+			WHERE TempDP.annee = annee_cible
+		) DP ON D.code = DP.code_departement
+		JOIN (
+			SELECT TempD.code_region, COUNT(*) AS departement_count -- Nombre total de département pour chaque région
+			FROM departement TempD
+			GROUP BY TempD.code_region
+		) DR ON D.code_region = DR.code_region
+		GROUP BY D.code_region, departement_count
+		HAVING COUNT(*) = departement_count;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- Procédure pour insérer les données dans les tables 'departement_pop'/'region_pop'. Elle utilise les fonctions définies plutôt
+CREATE OR REPLACE PROCEDURE insert_population_if_all(annee_cible VARCHAR)
 AS $$
 BEGIN
-    -- Mise à jour des populations des départements
-    UPDATE departement_pop AS DP
-    SET population = GDP.population
-    FROM (
+    -- Insertion des populations des départements
+    INSERT INTO departement_pop (code_departement, annee, population)
+        SELECT * FROM calcul_pop_dep(annee_cible)
+        ON CONFLICT DO NOTHING;
 
-    )
-    WHERE DP.code_departement = GDP.code_departement AND DP.annee = GDP.annee;
-
-    -- -- Mise à jour des populations des régions
-    UPDATE region_pop AS RP
-    SET population = GRP.population
-    FROM get_pop_region GRP
-    WHERE RP.code_region = GRP.code_region AND RP.annee = GRP.annee;
+    -- Insertion des populations des régions
+    INSERT INTO region_pop (code_region, annee, population)
+        SELECT * FROM calcul_pop_reg(annee_cible)
+        ON CONFLICT DO NOTHING;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION call_update_population_if_all()
+-- Fonction pour appeler la procédure
+CREATE OR REPLACE FUNCTION call_insert_population_if_all()
 RETURNS TRIGGER AS $$
+DECLARE
+   row record;
 BEGIN
-    CALL update_population_if_all();
+    FOR row IN SELECT DISTINCT annee_debut AS annee FROM statistiques_pop LOOP
+        CALL insert_population_if_all(row.annee);
+    END LOOP;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER population_update_departement_region
+-- Trigger pour détecter si une nouvelle année de recensement est ajoutée au niveau de la table 'statistiques_pop'
+CREATE OR REPLACE TRIGGER population_insert_departement_region
 AFTER INSERT ON statistiques_pop
 FOR EACH STATEMENT
-EXECUTE PROCEDURE call_update_population_if_all();
+EXECUTE PROCEDURE call_insert_population_if_all();
--- pages/triggers_suite.py
+++ pages/triggers_suite.py
@@ -1,7 +1,113 @@
 from dash import html, register_page
+from dash import callback, Input, Output, State, ctx
 import dash_mantine_components as dmc
+
+from dash.dash_table import DataTable
+from data.db_connector import execute_sql
+from data.data_processing import SQL_PATH, get_file_content
 
 register_page(__name__, path='/triggers_suite', name='Suite', title='Triggers suite', order=6, category_name='Triggers')
 
+REQUEST_DEP = '''SELECT D.code, D.nom, DP.annee, DP.population FROM departement_pop DP JOIN departement D ON D.code = DP.code_departement WHERE annee = '2024';'''
+REQUEST_REG = '''SELECT R.code, R.nom, RP.annee, RP.population FROM region_pop RP JOIN region R ON R.code = RP.code_region WHERE annee = '2024';'''
+
 def layout():
-    return 'C\'est bien vide ici :('
+    return html.Div(
+        [
+            result_table(),
+            code_box()
+        ],
+        id='triggers_suite_layout'
+    )
+
+def result_table():
+    df_dep = execute_sql(REQUEST_DEP)
+    df_reg = execute_sql(REQUEST_REG)
+    return html.Div(
+        [
+            dmc.Title('Insertion de données dans un département', order=4, transform='uppercase', style={'margin': '0 auto'}),
+            dmc.Text('L\'insertion se fera en 2024 et la population sera égale à 1 pour chaque commune.', style={'margin': '0 auto'}),
+            html.Div(
+                [
+                    dmc.Select(
+                        id='triggers_suite_select',
+                        value='33',
+                        searchable=True,
+                        data=execute_sql('SELECT code, nom FROM departement;').rename(columns={'code': 'value', 'nom': 'label'}).to_dict('records'),
+                        style={'width': 200},
+                    ),
+                    dmc.Button('INSÉRER', id='triggers_suite_button_insert', n_clicks=0)
+                ],
+                id='triggers_suite_buttons_groups'
+            ),
+            DataTable(
+                data=df_dep.to_dict('records'),
+                columns=[{'id': col, 'name': col} for col in df_dep.columns],
+                page_size=10,
+                style_table={
+                    'border-left': '1px solid rgb(233, 236, 239)',
+                    'border-right': '1px solid rgb(233, 236, 239)'
+                },
+                style_cell={'textAlign': 'left'},
+                style_as_list_view=True,
+                id='triggers_suite_table_departement'
+            ),
+            DataTable(
+                data=df_reg.to_dict('records'),
+                columns=[{'id': col, 'name': col} for col in df_reg.columns],
+                page_size=10,
+                style_table={
+                    'border-left': '1px solid rgb(233, 236, 239)',
+                    'border-right': '1px solid rgb(233, 236, 239)'
+                },
+                style_cell={'textAlign': 'left'},
+                style_as_list_view=True,
+                id='triggers_suite_table_region'
+            )
+        ],
+        id='triggers_suite_result_table'
+    )
+
+def code_box():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    dmc.Prism(
+                        get_file_content(f'{SQL_PATH}triggers_suite.sql'),
+                        language='sql',
+                        withLineNumbers=True,
+                        id='triggers_suite_code_highlight'
+                    )
+                ],
+                id='triggers_suite_code_box',
+                className='layout_code_highlight'
+            )
+        ],
+        id='triggers_suite_code_box_box'
+    )
+
+@callback(
+    [
+        Output('triggers_suite_table_departement', 'data'),
+        Output('triggers_suite_table_region', 'data'),
+    ],
+    [
+        Input('triggers_suite_button_insert', 'n_clicks')
+    ],
+    [
+        State('triggers_suite_select', 'value')
+    ],
+    prevent_initial_call=True)
+def table_update_triggers_insert(in_btn: int, state_select: int) -> tuple:
+    df_com_from_dep = execute_sql('SELECT code FROM commune WHERE code_departement = %s;', [state_select])
+    str_builder = 'INSERT INTO statistiques_pop (code_commune, type_statistique, annee_debut, annee_fin, valeur) VALUES\n'
+    str_builder += ('(\'' + df_com_from_dep['code'] + '\', \'population\', \'2024\', \'2024\', 1)').str.cat(sep=',\n')
+    str_builder += '\nON CONFLICT DO NOTHING;'
+
+    execute_sql(str_builder)
+
+    df_dep = execute_sql(REQUEST_DEP)
+    df_reg = execute_sql(REQUEST_REG)
+    
+    return df_dep.to_dict('records'), df_reg.to_dict('records')
